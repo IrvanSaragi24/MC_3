22da278e0e4c91fac1a104cddecc9aafd0a64dcd
IrvanSaragi24 <127461456+IrvanSaragi24@users.noreply.github.com>
2023-07-17 17:31:16 +0700

    try

---
+++ MC_3/view/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  MC_3
+//
+//  Created by Irvan P. Saragi on 17/07/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("IRvan")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
