89648eaca60b366f38eb6c4b40c04ae84d158d9b
Hanifah BN <ifhahnfh07@gmail.com>
2023-07-27 11:18:20 +0700

    Enable Background Running

--- MC_3/MC_3App.swift
+++ MC_3/MC_3App.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct MC_3App: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @State private var showSplash = true
    
     var body: some Scene {
@@ -28,5 +29,42 @@ struct MC_3App: App {
                     
             }
         }
+    }
+}
+
+// Implementing the AppDelegate
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Request notification authorization
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { _, _ in }
+        application.registerForRemoteNotifications()
+
+        // Set the UNUserNotificationCenter delegate
+        UNUserNotificationCenter.current().delegate = self
+
+        return true
+    }
+
+    // Handle notification tap when app is in the foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        if response.notification.request.identifier == "VoiceDetectingStopped" {
+            if let notificationFlag = response.notification.request.content.userInfo["notificationFlag"] as? Bool, notificationFlag {
+                // User tapped on the notification with the flag to run quizTime()
+                // Handle the next operation or open the next screen here
+                // For example, you can present the ListenView or perform the quizTime() method
+                // You might need to check the app state (background/foreground) here to proceed accordingly.
+
+                // Note: To present the ListenView, you may need to navigate to the correct navigation stack.
+                // For example, if your app uses a coordinator pattern, you can ask the coordinator to show the ListenView.
+
+                // Remove the flag after processing
+                UserDefaults.standard.removeObject(forKey: "QuizTimeFromNotification")
+                UserDefaults.standard.synchronize()
+            } else {
+                // Handle other types of notifications (if any)
+            }
+        }
+
+        completionHandler()
     }
 }
