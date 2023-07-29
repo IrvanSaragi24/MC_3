32b309723af7beac029596f1fedc81c5bbdc8aaa
Hanifah BN <ifhahnfh07@gmail.com>
2023-07-29 16:22:22 +0700

    Fixed Main Branch with AudioPlayer Attached

--- MC_3/View/ResultView.swift
+++ MC_3/View/ResultView.swift
@@ -18,7 +18,7 @@ struct ResultView: View {
         UIScreen.main.bounds.height
     }
     //    @State private var AnswerNo : Bool = false
-    @StateObject var synthesizerViewModel = SynthesizerViewModel()
+    @StateObject var playerViewModel = PlayerViewModel()
     @EnvironmentObject var lobbyViewModel: LobbyViewModel
     @EnvironmentObject private var multipeerController: MultipeerController
     @EnvironmentObject private var playerData: PlayerData
@@ -76,7 +76,7 @@ struct ResultView: View {
                             .padding(.bottom, 55)
                     }
                     .padding(.bottom, 250)
-                    Text(multipeerController.isWin ? "\(multipeerController.currentPlayer) Here \n\(multipeerController.currentPlayer) Hears" : "Find a New \nFriend" )
+                    Text(multipeerController.isWin ? "\(multipeerController.currentPlayer)'s Here \n\(multipeerController.currentPlayer) Hears" : "Find a New \nFriend" )
                         .font(.system(size: 32, design: .rounded))
                         .fontWeight(.bold)
                         .foregroundColor(Color("Second"))
@@ -88,14 +88,14 @@ struct ResultView: View {
                             .overlay {
                                 
                                 if multipeerController.isPlayer {
-                                    Text(multipeerController.isWin ? "Keren banget lo! Beneran perhatiin yak ternyata 😆" : "Eh lo! Lain kali perhatikan yak 🤬!")
+                                    Text(multipeerController.isWin ? "Keren banget! Kamu beneran perhatiin yak ternyata. 😆" : "Ah, gimana, deh! Gak asik banget nongkrong tapi gak dengerin. 🤬!")
                                          .font(.system(size: 17, design: .rounded))
                                          .fontWeight(.medium)
                                          .multilineTextAlignment(.center)
                                          .padding()
                                 }
                                 else {
-                                    Text(multipeerController.isWin ? "Hey, teman kamu mendengarkan dengan baik, ayo traktir dia kopi susu gula aren" : "Lu cari temen baru aja breeee")
+                                    Text(multipeerController.isWin ? "Hey, teman kamu mendengarkan dengan baik! Ayo traktir dia kopi susu gula aren!" : "Mendingan kamu cari temen baru aja, deh. Dia gak asik, ga dengerin pembicaraan!")
                                         .font(.system(size: 17, design: .rounded))
                                         .fontWeight(.medium)
                                         .multilineTextAlignment(.center)
@@ -135,10 +135,10 @@ struct ResultView: View {
                     Button {
                         print("Repeat Sound")
                         if multipeerController.isPlayer {
-                            synthesizerViewModel.startSpeaking(spokenString: multipeerController.isWin ? "Keren banget lo! Beneran perhatiin yak ternyata 😆" : "Eh lo! Lain kali perhatikan yak 🤬!")
+                            playerViewModel.playAudio(fileName: multipeerController.isWin ? "WinPlayer" : "LosePlayer")
                         }
                         else{
-                            synthesizerViewModel.startSpeaking(spokenString: multipeerController.isWin ? "Hey, teman kamu mendengarkan dengan baik! Ayo traktir dia kopi susu gula aren." : "Lu cari temen baru aja breeee.")
+                            playerViewModel.playAudio(fileName: multipeerController.isWin ? "WinReferee" : "LoseReferee")
                         }
                     } label: {
                         ZStack{
@@ -184,18 +184,15 @@ struct ResultView: View {
                 .padding(.bottom, screenHeight * 0.85)
                 .padding(.leading, screenWidth * 0.7)
             }
-            .onDisappear{
-                synthesizerViewModel.stopSpeaking()
-            }
             .onAppear() {
                 if multipeerController.lobby.numberOfQuestion == multipeerController.lobby.currentQuestionIndex {
                     isDoneAllQuestion = true
                 }
                 if multipeerController.isPlayer {
-                    synthesizerViewModel.startSpeaking(spokenString: multipeerController.isWin ? "Keren banget lo! Beneran perhatiin yak ternyata 😆" : "Eh lo! Lain kali perhatikan yak 🤬!")
+                    playerViewModel.playAudio(fileName: multipeerController.isWin ? "WinPlayer" : "LosePlayer")
                 }
                 else{
-                    synthesizerViewModel.startSpeaking(spokenString: multipeerController.isWin ? "Hey, teman kamu mendengarkan dengan baik! Ayo traktir dia kopi susu gula aren." : "Lu cari temen baru aja breeee")
+                    playerViewModel.playAudio(fileName: multipeerController.isWin ? "WinReferee" : "LoseReferee")
                 }
             }
         }
